c8fbc8ba2f52f13a3f867e290660edb3f1d3f36f
Luiz Eduardo <l.eduardo8316@gmail.com>
2023-11-20 17:15:35 -0300

    refactor: hash candidate password

--- src/main/java/br/com/luizeduu/vacancy_management/modules/candidate/usecase/CreateCandidateUseCase.java
+++ src/main/java/br/com/luizeduu/vacancy_management/modules/candidate/usecase/CreateCandidateUseCase.java
@@ -7,12 +7,16 @@ import br.com.luizeduu.vacancy_management.exceptions.UserFoundException;
 import br.com.luizeduu.vacancy_management.modules.candidate.dto.CreateCandidateDTO;
 import br.com.luizeduu.vacancy_management.modules.candidate.entity.Candidate;
 import br.com.luizeduu.vacancy_management.modules.candidate.repository.CandidateRepository;
+import br.com.luizeduu.vacancy_management.provider.JWTProvider;
 
 @Service
 public class CreateCandidateUseCase {
 
   @Autowired
   private CandidateRepository candidateRepository;
+
+  @Autowired
+  private JWTProvider jwtProvider;
 
   public Candidate execute(CreateCandidateDTO candidateDTO) {
     this.candidateRepository.findByUsernameOrEmail(candidateDTO.getUsername(), candidateDTO.getEmail())
@@ -24,7 +28,7 @@ public class CreateCandidateUseCase {
         .name(candidateDTO.getName())
         .username(candidateDTO.getUsername())
         .email(candidateDTO.getEmail())
-        .password(candidateDTO.getPassword())
+        .password(jwtProvider.hashPassword(candidateDTO.getPassword()))
         .description(candidateDTO.getDescription()).build();
 
     this.candidateRepository.save(candidate);
