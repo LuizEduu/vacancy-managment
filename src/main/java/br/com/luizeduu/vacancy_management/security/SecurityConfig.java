6384cf9aaa139ff7ac9a7ef73f5527d31f377a06
Luiz <l.eduardo8316@gmail.com>
2023-12-08 21:32:14 -0300

    feat: add actuator in project

--- src/main/java/br/com/luizeduu/vacancy_management/security/SecurityConfig.java
+++ src/main/java/br/com/luizeduu/vacancy_management/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -28,20 +29,21 @@ public class SecurityConfig {
   @Autowired
   private SecurityCandidateFilter securityCandidateFilter;
 
-  private static final String[] SWAGGER_LIST = {
+  private static final String[] PERMIT_ALL_LIST = {
       "/swagger-ui/**",
       "/v3/api-docs/**",
-      "/swagger-resources/**"
+      "/swagger-resources/**",
+      "/actuator/**"
   };
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.csrf(csrf -> csrf.disable())
+    httpSecurity.csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> {
           auth.requestMatchers("/candidate").permitAll().requestMatchers("/company").permitAll()
               .requestMatchers("/auth/company").permitAll()
               .requestMatchers("/auth/candidate").permitAll()
-              .requestMatchers(SWAGGER_LIST).permitAll();
+              .requestMatchers(PERMIT_ALL_LIST).permitAll();
 
           auth.anyRequest().authenticated();
         })
